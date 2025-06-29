2852bcd7ccc22d690f01d334c3249abcc5c9f2c3
Bully26 <priyashupradhan26@gmail.com>
2025-06-29 16:20:09 +0530

    successfull integration/verfication with portforwarding in strip

--- payment/server/api/checkout-session.ts
+++ payment/server/api/checkout-session.ts
@@ -1,50 +1,26 @@
-// import Stripe from 'stripe'
-
-// const stripe = new Stripe('sk_test_51RejxWAcowfCPm4r9g49aMh2kI7lncO6T0OYD6hCWiWeN1d778XbGFIePJEe3QqH5SRnLVkZ60Zjj9AxLGw3piil001FjAloYi', {
-//   apiVersion: '2024-04-10'
-// })
-
-// export default defineEventHandler(async (event) => {
-//   const session = await stripe.checkout.sessions.create({
-//     payment_method_types: ['card'],
-//     mode: 'payment',
-//     line_items: [
-//       {
-//         price_data: {
-//           currency: 'usd',
-//           product_data: { name: 'Test Product' },
-//           unit_amount: 1000
-//         },
-//         quantity: 1
-//       }
-//     ],
-//     success_url: 'http://localhost:3000/success',
-//     cancel_url: 'http://localhost:3000/cancel',
-//     metadata: { userId: 'abc123' } // optional
-//   })
-
-//   return { url: session.url }
-// })
-
-// // server/api/checkout-session.post.ts
 import Stripe from 'stripe'
 import { defineEventHandler, readBody } from 'h3'
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
-  const stripe = new Stripe(process.stripesk, {
-    apiVersion: '2024-04-10'
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: '2024-04-10',
   })
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    line_items: [{ price:**your custom priceid**, quantity: 1 }],
-    mode: 'payment',
-    success_url: 'http://localhost:3000/success',
-    cancel_url: 'http://localhost:3000/cancel'
+    line_items: [
+      {
+        price: , 
+        quantity: 1,
+      },
+    ],
+    mode:, 
+    success_url: , 
+    cancel_url: ,   
   })
 
-  console.log(session.url);
+  console.log(session.url)
   return { id: session.id }
 })
